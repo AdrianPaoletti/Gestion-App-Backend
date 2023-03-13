4a7883e83f2840dc536c6b134d2c26864a57e92f
AdrianPaoletti <adrian.paoletti@bideaavant.com>
2023-03-13 17:22:54 +0100

    znz

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,18 @@
-import dotenv from "dotenv";
-dotenv.config();
-import { initilizeServer } from "./server/index";
-import connectDB from "./database/index";
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const index_1 = require("./server/index");
+const index_2 = __importDefault(require("./database/index"));
 const port = process.env.PORT ?? process.env.SERVER_PORT ?? 6001;
+window.exports = {};
 (async () => {
     try {
-        await connectDB(process.env.MONGODB_STRING);
-        initilizeServer(+port);
+        await (0, index_2.default)(process.env.MONGODB_STRING);
+        (0, index_1.initilizeServer)(+port);
     }
     catch (error) {
         process.exit(1);
--- dist/server/index.js
+++ dist/server/index.js
@@ -1,29 +1,36 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import chalk from "chalk";
-import Debug from "debug";
-import userRoutes from "./routes/userRoutes";
-const debug = Debug("app:server");
-const app = express();
-app.use(cors());
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = exports.initilizeServer = void 0;
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
+const chalk_1 = __importDefault(require("chalk"));
+const debug_1 = __importDefault(require("debug"));
+const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+const debug = (0, debug_1.default)("app:server");
+const app = (0, express_1.default)();
+exports.app = app;
+app.use((0, cors_1.default)());
 const initilizeServer = (port) => new Promise((resolve, reject) => {
     const server = app.listen(port, () => {
-        debug(chalk.green(`Listening on port ${port}`));
+        debug(chalk_1.default.green(`Listening on port ${port}`));
         resolve(server);
     });
     server.on("error", (error) => {
-        debug(chalk.red("An error with the server occurred"));
+        debug(chalk_1.default.red("An error with the server occurred"));
         if (error.code === "EADDRINUSE") {
-            debug(chalk.red(`Port ${port} is in use`));
+            debug(chalk_1.default.red(`Port ${port} is in use`));
         }
         reject();
     });
     server.on("close", () => {
-        debug(chalk.yellow("Server disconnected."));
+        debug(chalk_1.default.yellow("Server disconnected."));
     });
 });
-app.use(morgan("dev"));
-app.use(express.json());
-app.use("/users", userRoutes);
-export { initilizeServer, app };
+exports.initilizeServer = initilizeServer;
+app.use((0, morgan_1.default)("dev"));
+app.use(express_1.default.json());
+app.use("/users", userRoutes_1.default);
